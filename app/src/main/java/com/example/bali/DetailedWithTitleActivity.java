8de7989c475ad5faeea61c6888169c368e5f6005
GeorgiaTsironi <gtsironi@qualco.eu>
2023-08-01 11:56:49 +0300

    replace

--- app/src/main/java/com/example/bali/DetailedWithTitleActivity.java
+++ app/src/main/java/com/example/bali/DetailedWithTitleActivity.java
@@ -45,7 +45,7 @@ public class DetailedWithTitleActivity extends AppCompatActivity {
 
     String textForMaps = "";
 
-    Boolean isMapsButtonHiddeen = false;
+    Boolean isMapsButtonHidden = false;
     boolean isEditMode = false;
 
     TextView labelTextView;
@@ -93,181 +93,24 @@ public class DetailedWithTitleActivity extends AppCompatActivity {
             case Hotels:
                 setContentView(R.layout.activity_hotels);
 
-                ImageButton ubudHotelMap = findViewById(R.id.imagebutton_hotels_ubudmap);
-                ubudHotelMap.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_ubud_map))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_ubudmap), getResources().getString(R.string.hotel_ubud_map));
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_ubudlink), getResources().getString(R.string.hotel_ubud_link));
 
-                ImageButton ubudHotelLink = findViewById(R.id.imagebutton_hotels_ubudlink);
-                ubudHotelLink.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_ubud_link))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_uluwatumap), getResources().getString(R.string.hotel_uluwatu_map));
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_uluwatulink), getResources().getString(R.string.hotel_uluwatu_link));
 
-                ImageButton uluwatuHotelMap = findViewById(R.id.imagebutton_hotels_uluwatumap);
-                uluwatuHotelMap.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_uluwatu_map))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_singaporemap), getResources().getString(R.string.hotel_singapore_map));
+                buttonWithLink(findViewById(R.id.imagebutton_hotels_singaporelink), getResources().getString(R.string.hotel_singapore_link));
 
-                ImageButton uluwatuHotelLink = findViewById(R.id.imagebutton_hotels_uluwatulink);
-                uluwatuHotelLink.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_uluwatu_link))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                ImageButton singaporeHotelMap = findViewById(R.id.imagebutton_hotels_singaporemap);
-                singaporeHotelMap.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_singapore_map))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                ImageButton singaporeHotelLink = findViewById(R.id.imagebutton_hotels_singaporelink);
-                singaporeHotelLink.setOnClickListener(v -> {
-                    try {
-                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.hotel_singapore_link))));
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                isMapsButtonHiddeen = true;
+                isMapsButtonHidden = true;
 
                 break;
-
-            /*case Churches:
-                text = getResources().getString(R.string.churchesText);
-                titleToolbarText = "Εκκλησίες";
-
-                textForMaps = "churches";
-
-                items.add(new PlaceItem(this, R.string.oudeKerk, R.drawable.oude_kerk, null, DetailedActivity.Places.OudeKerk));
-                items.add(new PlaceItem(this, R.string.westerkerk, R.drawable.westerkerk, null, DetailedActivity.Places.Westerkerk));
-
-                break;
-
-            case Parks:
-                text = getResources().getString(R.string.parksText);
-                titleToolbarText = "Πάρκα";
-
-                textForMaps = "parks";
-
-                items.add(new PlaceItem(this, R.string.vondelpark, R.drawable.vondelpark, null, DetailedActivity.Places.VondelPark));
-
-                break;
-
-            case Brunch:
-                //text = getResources().getString(R.string.parksText);
-                titleToolbarText = "Brunch";
-
-                textForMaps = "brunch";
-
-                items.add(new PlaceItem(this, R.string.pluk, R.drawable.pluk, null, DetailedActivity.Places.Pluk));
-                items.add(new PlaceItem(this, R.string.omelegg, R.drawable.omelegg, null, DetailedActivity.Places.Omelegg));
-                items.add(new PlaceItem(this, R.string.breakfastClub, R.drawable.breakfast_club, null, DetailedActivity.Places.BreakfastClub));
-                items.add(new PlaceItem(this, R.string.bakersAndRoasters, R.drawable.bakers_roasters, null, DetailedActivity.Places.BakersRoasters));
-                items.add(new PlaceItem(this, R.string.greenwoods, R.drawable.greenwoods, null, DetailedActivity.Places.Greenwoods));
-                items.add(new PlaceItem(this, R.string.staringAtJacob, R.drawable.staring_at_jacob, null, DetailedActivity.Places.StaringAtJacob));
-                items.add(new PlaceItem(this, R.string.pancakeBakery, R.drawable.pancake_bakery, null, DetailedActivity.Places.PancakeBakery));
-                items.add(new PlaceItem(this, R.string.scandinavianEmbassy, R.drawable.scandinavian_embassy7, null, DetailedActivity.Places.ScandinavianEmbassy));
-                items.add(new PlaceItem(this, R.string.littleCoins, R.drawable.little_coins, null, DetailedActivity.Places.LittleCoins));
-                items.add(new PlaceItem(this, R.string.bakhuysAmsterdam, R.drawable.bakhuys_amsterdam, null, DetailedActivity.Places.BakhuysAmsterdam));
-
-                break;
-
-            case Food:
-                //toolBarLayout.setTitle(getResources().getString(R.string.food));
-                text = getResources().getString(R.string.foodText);
-
-//                textForMaps = "museums";
-
-                break;
-
-            case Dessert:
-
-                titleToolbarText = "Γλυκά";
-
-                textForMaps = "dessert";
-
-                items.add(new PlaceItem(this, R.string.vanStapele, R.drawable.van_stapele, null, DetailedActivity.Places.VanStapele));
-
-                break;
-
-            case Drink:
-
-                text = getResources().getString(R.string.drinkText);
-                titleToolbarText = "Ποτό";
-
-                items.add(new PlaceItem(this, R.string.xtracoldIcebar, R.drawable.xtracold, null, DetailedActivity.Places.XtraCold));
-
-                textForMaps = "drink";
-
-                break;
-
-            case Shopping:
-
-                text = getResources().getString(R.string.shoppingText);
-                titleToolbarText = "Ψώνια";
-
-                items.add(new PlaceItem(this, R.string.kalverstraat, R.drawable.kalverstraat, null, DetailedActivity.Places.Kalvestraat));
-                items.add(new PlaceItem(this, R.string.nineStreets, R.drawable.nine_streets, null, DetailedActivity.Places.NineStreets));
-                items.add(new PlaceItem(this, R.string.albertCuyp, R.drawable.albert_cuyp, null, DetailedActivity.Places.AlbertCuyp));
-
-                textForMaps = "markets";
-
-                break;
-
-            case Other:
-
-                text = "";
-                titleToolbarText = "Άλλα";
-
-                items.add(new PlaceItem(this, R.string.centralStation, R.drawable.centraal_station, null, DetailedActivity.Places.CentralStation));
-                items.add(new PlaceItem(this, R.string.adamLookout, R.drawable.adam_lookout, null, DetailedActivity.Places.AdamLookout));
-                items.add(new PlaceItem(this, R.string.nemoRooftop, R.drawable.nemo, null, DetailedActivity.Places.Nemo));
-
-                break;
-
-            case Neighborhood:
-
-                text = "";//getResources().getString(R.string.shoppingText);
-                titleToolbarText = "Γειτονιές";
-
-                items.add(new PlaceItem(this, R.string.jordaan, R.drawable.jordaan, null, DetailedActivity.Places.Jordaan));
-                items.add(new PlaceItem(this, R.string.museumQuarter, R.drawable.museum_quarter, null, DetailedActivity.Places.MuseumQuarter));
-                items.add(new PlaceItem(this, R.string.damSquare, R.drawable.dam_square, null, DetailedActivity.Places.DamSquare));
-
-                //textForMaps = ;
-
-                break;*/
 
             case Language:
 
                 text = getResources().getString(R.string.languageText);
                 titleToolbarText = getResources().getString(R.string.language);
-                isMapsButtonHiddeen = true;
+                isMapsButtonHidden = true;
 
                 break;
 
@@ -275,7 +118,7 @@ public class DetailedWithTitleActivity extends AppCompatActivity {
 
                 text = getResources().getString(R.string.flights_infoText);
                 titleToolbarText = getResources().getString(R.string.flights_info);
-                isMapsButtonHiddeen = true;
+                isMapsButtonHidden = true;
 
                 break;
         }
@@ -293,6 +136,18 @@ public class DetailedWithTitleActivity extends AppCompatActivity {
         initFabs();
     }
 
+    private void buttonWithLink(ImageButton ViewById, String String) {
+
+        ViewById.setOnClickListener(v -> {
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String)));
+            }
+            catch (ActivityNotFoundException e){
+                Toast.makeText(DetailedWithTitleActivity.this, "No app found for handle this Url", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private void initFabs() {
 
         FloatingActionButton fabMaps = findViewById(R.id.fab_detailed_maps);
@@ -304,7 +159,7 @@ public class DetailedWithTitleActivity extends AppCompatActivity {
         fabMaps.setOnClickListener(view -> openGoogleMaps());
         fabEdit.setOnClickListener(view -> edit(fabEdit));
 
-        if (isMapsButtonHiddeen)
+        if (isMapsButtonHidden)
             fabMaps.setVisibility(View.INVISIBLE);
     }
 
